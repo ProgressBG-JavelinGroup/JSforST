a08f930b937e7a8f3f139e7f7177814eedc004aa
ProgressBG-JavelinGroup <ProgressBG.WWW.Courses+ssjs@gmail.com>
2017-07-24 22:14:58 +0300

    Restructuring

--- pages/themes/JS_BasicSyntax_1/examples/labs/index.js
+++
@@ -1,5 +0,0 @@
-var x;
-for ( ;  ;  ) {
-	x = x - 2
-	console.log(x);
-};
--- pages/themes/JS_Objects/labs/objects.js
+++
@@ -1,8 +0,0 @@
-
-
-
-var tom = new Pet("Tom", "cat", 3, "fish");
-var jerry = new Pet("Jerry", "mouse", 2, "cheese");
-
-console.dir(tom);
-console.dir(jerry);
